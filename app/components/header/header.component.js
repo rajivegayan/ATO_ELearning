5dcb457885d8d12d36dcd3a3f5576f3da04e1724
Rajive Gayan <rajivegayan@gmail.com>
2021-07-16 02:01:47 +0930

    Final system completion with angular data / dependency injection to user

--- app/components/header/header.component.js
+++ app/components/header/header.component.js
@@ -1,28 +1,22 @@
-angular.module('headerComponent', ['ngRoute']);
+angular.module('headerComponent', ['ngRoute', ]);
 
 angular.
     module('headerComponent').
     component('headerComponent', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
         templateUrl: 'components/header/header.template.html',
-        /*controller: ['$http', 
-        function phoneListController($http) {
+        controller: function headerComponentController($scope, $rootScope, $http) {        
           var self = this;          
-          $http.get('phones/phones.json').then(function(response) {
-            console.log(response);
-            // self.phones = response.data.slice(0,5);
-            self.phones = response.data;
-          });          
-          //this.orderProp = 'age'; // Setting up the default value
+          $http.get('./data/users/users.json').then(function(response) {
+            
+            //console.log(response.data);
+            var userData = response.data;
+            
 
-        }]*/
-        // controller: ['headerComponent', 
-        // function phoneListController(Phone) {        
-        // //   this.phones = Phone.query();
-        // //   this.orderProp = 'age';
-        // }]
-        controller: function headerComponentController($scope, $rootScope) {        
-        //   this.phones = Phone.query();
-        //   this.orderProp = 'age';
-        $scope.userName = "Rajive";
+            // Selecting a random user between 3 users
+            var randomUser = Math.floor(Math.random() * 3);
+            $scope.userName = userData[randomUser]["firstName"];
+            $scope.profilePicture = userData[randomUser]["profileImage"];
+            //$scope.userName = "Rajive";
+          });          
         }
     });
